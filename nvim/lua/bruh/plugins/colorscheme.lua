6ac6836cc4038d8f96bb1d3040b62d6f946cf80b
bruhngl0 <adityashrm500@gmail.com>
2025-04-01 13:40:42 +0530

    defaultsetting

--- nvim/lua/bruh/plugins/colorscheme.lua
+++ nvim/lua/bruh/plugins/colorscheme.lua
@@ -15,12 +15,10 @@ return {
 		"rose-pine/neovim",
 		name = "rose-pine",
 		lazy = false,
-		priority = 1000,
 		config = function()
 			require("rose-pine").setup({
 				variant = "dawn", -- light theme
 			})
-			vim.cmd.colorscheme("rose-pine")
 		end,
 	},
 	"eldritch-theme/eldritch.nvim",
